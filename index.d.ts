1714e4c43d1b188bc0a22cafbac788bdc1475d65
Christophe Hurpeau <christophe@hurpeau.com>
2018-05-12 11:03:42 +0200

    docs: fix typings

--- index.d.ts
+++ index.d.ts
@@ -1,10 +1,10 @@
 declare module "error-html" {
   export interface Options {
     fileProtocol?: 'file';
-  };
+  }
 
   export default class HtmlRenderer {
     constructor(options?: Options);
-    render(error: Error);
+    render(error: Error): string;
   }
 }
